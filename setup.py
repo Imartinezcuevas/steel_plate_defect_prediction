955b2557b60821913f26a871183e54553042f9b7
Iván Martínez Cuevas <imartinezcuevas@gmail.com>
2024-03-13 09:21:51 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict the probability of various defects on steel plates.',
+    author='Ivan Martinez',
+    license='MIT',
+)
